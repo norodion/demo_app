6756ac4f8d148678a669aa465bc6212bce55e490
norodion <norodion@gmail.com>
2011-11-04 22:56:47 +0600

    Pervi commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '14d24721fe68b224f0a5610af540e3e893476a01543b747d0882736fac5456902145f710e6ba9b645815ee0930dc77873dfd8795fa9b0d568d2177c1a3623ada'
